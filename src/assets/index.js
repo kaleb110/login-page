eb2400be2c0c03dda0dd629b2f144b3312f65e92
kaleb110 <137324547+kaleb110@users.noreply.github.com>
2024-05-15 16:19:11 +0300

    latest commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,7 @@
 import facebook from "./facebook.svg";
 import twitter from "./twitter.svg";
 import linkedin from "./linkedin.svg";
+import email from "./email.svg";
+import password from "./password.svg";
 
-export { facebook, twitter, linkedin };
+export { facebook, twitter, linkedin, email, password };
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,54 +1,75 @@
 // import Input from "./Input";
-import { facebook, twitter, linkedin } from "../assets";
+import { facebook, twitter, linkedin, email, password } from "../assets";
 
 const Hero = () => (
   <div className="bg-gradient min-h-screen">
-    <div className="flex justify-center py-14">
-      <div className="flex flex-col justify-center items-center gap-4 border border-black max-sm:padding-x padding-y bg-black w-[50%] max-md:w-[60%]">
-        <h1 className="text-white text-3xl">LOGIN</h1>
-        <p className="text-slate-500 font-normal pb-4 text-center">
+    <div className="flex justify-center py-4 sm:py-6 lg:py-7 xl:py-10">
+      <div className="flex flex-col justify-center items-center gap-4 lg:gap-6 xl:gap-12 px-5 sm:px-10 py-12 sm:py-16 bg-black w-[70%] md:w-[45%] xl:w-[35%] rounded-xl">
+        <h1 className="text-white text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold">
+          LogIn
+        </h1>
+        <p className="text-slate-500 max-sm:text-sm lg:text-lg xl:text-xl font-normal pb-2 text-center">
           Please enter your login and password
         </p>
         {/* <Input placeholder={`Username`} /> */}
-        <div>
-          <input type="text" className="input" placeholder="Username" />
+        <div className="relative">
+          <img
+            src={email}
+            alt="Email Logo"
+            width={18}
+            height={18}
+            className="absolute left-6 top-[50%] translate-y-[-50%] "
+          />
+          <input type="text" className="input" placeholder="Email" />
         </div>
-        <div>
-          <input type="text" className="input" placeholder="Password" />
+        <div className="relative">
+          <img
+            src={password}
+            alt="Email Logo"
+            width={18}
+            height={18}
+            className="absolute left-6 top-[50%] translate-y-[-50%] "
+          />
+          <input type="password" className="input" placeholder="Password" />
         </div>
-        <div className="flex flex-col items-center justify-center ">
-          <a href="/" className="text-slate-500 font-normal pb-4 underline">
+        <div className="flex flex-col items-center justify-center">
+          <a
+            href="#"
+            className="text-slate-500 font-normal pb-6 lg:pb-10 underline text-sm lg:text-lg xl:text-xl"
+          >
             Forgot Password?
           </a>
 
-          <input
-            type="text"
-            className="inputLogin max-sm:w-full "
-            placeholder="Login"
-          />
+          <button className="inputLogin w-full">LOGIN</button>
         </div>
-        <div className="flex justify-center items-center gap-6 pt-6">
-          <img
-            src={facebook}
-            alt="Facebook Logo"
-            width={24}
-            height={24}
-            className="hover:cursor-pointer"
-          />
-          <img
-            src={twitter}
-            alt="Twitter Logo"
-            width={24}
-            height={24}
-            className="hover:cursor-pointer"
-          />
-          <img
-            src={linkedin}
-            alt="Linkedin Logo"
-            width={24}
-            height={24}
-            className="hover:cursor-pointer"
-          />
+        <div className="flex justify-center items-center gap-6 lg:gap-10 pt-4">
+          <a href="#">
+            <img
+              src={facebook}
+              alt="Facebook Logo"
+              width={24}
+              height={24}
+              className="hover:cursor-pointer"
+            />
+          </a>
+          <a href="#">
+            <img
+              src={twitter}
+              alt="Twitter Logo"
+              width={24}
+              height={24}
+              className="hover:cursor-pointer"
+            />
+          </a>
+          <a href="#">
+            <img
+              src={linkedin}
+              alt="Linkedin Logo"
+              width={20}
+              height={20}
+              className="hover:cursor-pointer"
+            />
+          </a>
         </div>
       </div>
     </div>
